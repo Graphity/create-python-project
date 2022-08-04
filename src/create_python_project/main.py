e93f6e233426395a6f6621d8bfd404823d61f5f2
Graphity <rohood3@gmail.com>
2022-08-04 23:49:38 +0400

    Fix os path join

--- src/create_python_project/main.py
+++ src/create_python_project/main.py
@@ -15,7 +15,7 @@ def create_python_project(name):
     os.mknod(os.path.join(src_path, "__init__.py"))
 
     if click.confirm("Do you want to add MIT license?"):
-        mit_license_template_path = os.join(MODULE_DIR, "MIT_LICENSE_TEMPLATE")
+        mit_license_template_path = os.path.join(MODULE_DIR, "MIT_LICENSE_TEMPLATE")
         with open(mit_license_template_path) as f:
             template = f.read()
         full_name = click.prompt("Enter full name for license")
